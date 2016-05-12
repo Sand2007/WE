ef9bbe57f4a51de4b9d9a283379822f0caf1ec05
Sand2007 <sand2007@i.ua>
2016-05-12 11:16:15 +0300

    add video page

--- js/app/videoModuleApp.js
+++ js/app/videoModuleApp.js
@@ -1,4 +1,5 @@
-﻿angular.module('videoModuleApp', [])
+﻿var videoModuleApp = angular.module('videoModuleApp', [])
+
 .controller('videoCtrl', function ($scope, $http, $sce) {
     var mainVideoId, intermediateVideoId, intermediateVideoDescription;
     $scope.loadVideo = function () {
@@ -6,11 +7,11 @@
         $http.get("../../data/videoPageData/video.json").success(function (response) {
             
 
-            mainVideoId = response[0].id;
-            $scope.mainVideo = $sce.trustAsResourceUrl('http://www.youtube.com/embed/' + mainVideoId);
-            $scope.mainVideoDescription = response[0].videoDescription;
-            response.splice(0, 1);
-            $scope.videos = response;
+            mainVideoId = response[0].id; //В переменную mainVideoId записываем нулевое по порядку значение id который пришел в json файле
+            $scope.mainVideo = $sce.trustAsResourceUrl('http://www.youtube.com/embed/' + mainVideoId); // Записываем в $scope.mainVideo адресс ссылки на youtube главного видео
+            $scope.mainVideoDescription = response[0].videoDescription;// Записываем в $scope.mainVideoDescription описание главного видео
+            response.splice(0, 1); //Удаляем из ответа пришедшего с сервера первый элемент(главное видео)
+            $scope.videos = response; // Записываем в $scope.videos ответ от сервера без первого видео(главного видео)
             }
             
        
@@ -18,16 +19,34 @@
     }();
 
     $scope.clickSecondVideo = function (video, index) {
-        intermediateVideoId = video.id;
-        $scope.mainVideo = $sce.trustAsResourceUrl('http://www.youtube.com/embed/' + video.id);        
-        $scope.videos[index].id = mainVideoId;
-        mainVideoId = intermediateVideoId;
+        intermediateVideoId = video.id; // в переменную intermediateVideoId записываем id видео по которому кликнули
+        $scope.mainVideo = $sce.trustAsResourceUrl('http://www.youtube.com/embed/' + video.id); // в $scope.mainVideo (главное видео) заносим видео, по которому кликнули       
+        $scope.videos[index + $scope.currentPage * $scope.pageSize].id = mainVideoId; // В массиве меняем всех видео меняем видео по которому кликнули на id главного видео | $scope.currentPage * $scope.pageSize к индексу добавляем размерность видео в данном случае 5
+        mainVideoId = intermediateVideoId; // в переменную mainVideoId  записываем записываем из буфера id по которому кликнули
 
         intermediateVideoDescription = $scope.mainVideoDescription // передаем в переменную описание главного ролика
         
         $scope.mainVideoDescription = video.videoDescription;
-        $scope.videos[index].videoDescription = intermediateVideoDescription;
+        $scope.videos[index + $scope.currentPage * $scope.pageSize].videoDescription = intermediateVideoDescription;
     }
 
+
+
+
+    $scope.currentPage = 0; // Устанавливаем начальную страницу
+    $scope.pageSize = 5; // Устанавливаем количество видео, которое будет отображаться на экране
+    
+    $scope.numberOfPages = function () {
+        return Math.ceil($scope.videos.length / $scope.pageSize);
+    }
+   
+
         
+});
+
+videoModuleApp.filter('startFrom', function () {
+    return function (input, start) {
+        start = +start; //parse to int
+        return input.slice(start);
+    }
 });
